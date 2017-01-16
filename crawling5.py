ee9b97e9a02feede656a943f6213cbfeba97222f
shizuru3 <yutaro.suzuki0509@gmail.com>
2017-01-16 22:43:31 +0900

    Modified crawler.

--- crawling5.py
+++ crawling5.py
@@ -50,4 +50,25 @@ def followExternalOnly(startingSite):
     print("Random external link is:" + externalLink)
     followExternalOnly(externalLink)
 
-followExternalOnly("http://oreilly.com")
+# このサイトで見つかったすべての外部URLのリストを集める
+allExtLinks = set()
+allIntLinks = set()
+def getAllExternalLinks(siteUrl):
+    html = urlopen(siteUrl)
+    domain = urlparse(siteUrl).scheme+"://"+urlparse(siteUrl).netloc
+    bsObj = BeautifulSoup(html,"lxml")
+    internalLinks = getInternalLinks(bsObj,domain)
+    externalLinks = getExternalLinks(bsObj,domain)
+
+    for link in externalLinks:
+        if link not in allExtLinks:
+            allExtLinks.add(link)
+            print(link)
+
+    for link in internalLinks:
+        if link not in allIntLinks:
+            allIntLinks.add(link)
+            getAllExternalLinks(link)
+
+allIntLinks.add("http://oreilly.com")
+getAllExternalLinks("http://oreilly.com")
